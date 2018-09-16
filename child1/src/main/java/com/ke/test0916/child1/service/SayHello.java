24e94683561259467de1aa25492561af249f8943
yukongsheng <1010315950@qq.com>
2018-09-16 19:58:48 +0800

    本地仓库第一次上传到远程仓库

---
+++ child1/src/main/java/com/ke/test0916/child1/service/SayHello.java
@@ -0,0 +1,7 @@
+package com.ke.test0916.child1.service;
+
+public class SayHello {
+    public String say(String name){
+        return "Hello" + name;
+    }
+}
---
+++ child2/src/main/java/com/ke/test0916/child2/service/Test.java
@@ -0,0 +1,8 @@
+package com.ke.test0916.child2.service;
+
+public class Test {
+    public String test(String name){
+//        SayHello sh = new SayHello();
+        return null;
+    }
+}
